01ef92fb957ab1c467023263b1ead29d4ce1eec3
fredrickemile <frederickauthur@hotmail.com>
2020-05-31 21:32:36 +0000

    created files

--- src/gaze_estimation.py
+++ src/gaze_estimation.py
@@ -1,7 +1,14 @@
+    
 '''
 This is a sample class for a model. You may choose to use it as-is or make any changes to it.
 This has been provided just to give you an idea of how to structure your model class.
 '''
+import cv2 
+import os 
+import sys 
+import logging as log 
+import math 
+from openvino.inference_engine import IECore 
 
 class Model_X:
     '''
@@ -9,9 +16,26 @@ class Model_X:
     '''
     def __init__(self, model_name, device='CPU', extensions=None):
         '''
-        TODO: Use this to set your instance variables.
+        DONE: Use this to set your instance variables.
         '''
-        raise NotImplementedError
+        self.model_name = model_name 
+        self.device = device 
+        self.extensions = extensions
+        self.network = None 
+        self.plugin = None 
+        self.exec_network = None  
+        self.infer_request = None 
+        self.input_name = None 
+        self.input_shape = None 
+        self.output_name = None 
+        self.output_shape = None 
+
+        self.model_structure = self.model_name 
+        self.model_weights = self.model_name.split('.')[0]+'.bin'
+
+
+
+        
 
     def load_model(self):
         '''
@@ -19,7 +43,11 @@ class Model_X:
         This method is for loading the model to the device specified by the user.
         If your model requires any Plugins, this is where you can load them.
         '''
-        raise NotImplementedError
+        self.plugin = IECore() 
+        self.network = self.plugin.read_network(model=self.model_structure, weights = self.model_weights) 
+        supported_layers  = self.plugin.query_network(network=self.network, device_name=self.device) 
+        not_supported_layer = [layers for layers in self.network.layers.keys() if layers not in supported_layers]
+        
 
     def predict(self, image):
         '''
